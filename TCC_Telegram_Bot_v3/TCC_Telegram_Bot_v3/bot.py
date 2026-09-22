51d70f6e10d79f9b3ff197eeead5271fecf202d8
iifaio <94977115+iifaio@users.noreply.github.com>
2026-09-23 02:07:32 +0300

    bot.py

--- TCC_Telegram_Bot_v3/TCC_Telegram_Bot_v3/bot.py
+++ TCC_Telegram_Bot_v3/TCC_Telegram_Bot_v3/bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
